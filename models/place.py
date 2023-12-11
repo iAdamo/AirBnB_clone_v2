cc4fa44c20960bb25507e3cb02bd0844507a9b37
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-12 00:40:06 +0900

    Update:DBStorage-review,user and place

--- models/place.py
+++ models/place.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""Place Module for HBNB project
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from models import storage
+from models.review import Review
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """A place to stay
+    """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -17,3 +22,19 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    reviews = relationship(
+        'Review',
+        backref='place',
+        cascade='all, delete-orphan')
+
+    @property
+    def reviews(self):
+        """getter attribute reviews that returns the list of Review instances
+        with place_id equals to the current Place.id
+        """
+        all_reviews = storage.all(Review)
+        place_reviews = []
+        for review in all_reviews.values():
+            if review.place_id == self.id:
+                place_reviews.append(review)
+        return place_reviews
--- models/user.py
+++ models/user.py
@@ -16,3 +16,7 @@ class User(BaseModel, Base):
         'Place',
         backref='user',
         cascade='all, delete-orphan')
+    reviews = relationship(
+        'Review',
+        backref='user',
+        cascade='all, delete-orphan')
