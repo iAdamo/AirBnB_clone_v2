b1649cc4ceece771c3eb6313aa6801260d71f745
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-13 01:19:24 +0900

    setup_web_static | remove sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,12 +28,31 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-search='location / {'
-replace='location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\t&'
-file_path='/etc/nginx/sites-available/default'
+#   search='location / {'
+#   replace='location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\t&'
+#   file_path='/etc/nginx/sites-available/default'
 
-sudo sed -i "s|$search|$replace|" "$file_path"
+#   sudo sed -i "s|$search|$replace|" "$file_path"
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+cat > /etc/nginx/sites-available/default << _EOL_
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html;
+    server_name _;
+    add_header X-Served-By \$HOSTNAME;
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+    }
+    location / {
+        rewrite ^/redirect_me https://www.github.com/iAdamo permanent;
+        error_page 404 /404.html;
+        try_files \$uri \$uri/ =404;
+    }
+}
+_EOL_
 
 # Restart nginx
 sudo service nginx restart
