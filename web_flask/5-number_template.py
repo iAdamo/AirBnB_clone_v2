6685c2407f1877a045e7b8711f815c37f913cb18
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-31 02:42:31 +0900

    task 5: Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""A python script that starts a Flask web app listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """displays 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display "HBNB"
+    """
+    return 'HBNB'
+
+
+@app.route('/c', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """display “C ” followed by the value of the text variable
+
+    Args:
+        text(string): Variable section to add to URL
+
+    Return:
+        variable content
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """Display "Python", followed by the value of the text variable
+
+    Args:
+        text(string): Variable section to add to URL
+
+    Return:
+        variable content
+    """
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """Display "n is a number", only if n is an integer
+
+    Args:
+        n(string): Variable section to add to URL
+
+    Return:
+        variable content
+    """
+    return f'{n} is a number'
+
+
+@app.route('/number_template', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
