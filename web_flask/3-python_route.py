656e7bd13e3c83833516453e861f8552a101bae6
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-30 20:41:04 +0900

    Task 3: python route variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""A python script that starts a Flask web app listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """displays 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display "HBNB"
+    """
+    return 'HBNB'
+
+
+@app.route('/c', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """display “C ” followed by the value of the text variable
+
+    Args:
+        text(string): Variable section to add to URL
+
+    Return:
+        variable content
+    """
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """Display "Python", followed by the value of the text variable
+
+    Args:
+        text(string): Variable section to add to URL
+
+    Return:
+        variable content
+    """
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
