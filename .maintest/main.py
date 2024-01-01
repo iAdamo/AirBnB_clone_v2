6e9748191b1427ccb05c5549677a14f5c71f4ddd
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-01-02 01:12:23 +0900

    List of states | AUTHORS

---
+++ .maintest/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ Test cities access from a state
+"""
+from models import storage
+from models.state import State
+from models.city import City
+
+"""
+ Objects creations
+"""
+state_1 = State(name="California")
+print("New state: {}".format(state_1))
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A python script that starts a Flask web app listening on 0.0.0.0, port 5000
+"""
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """A method to remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """A method to render an HTML page with a list of all State objects
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
